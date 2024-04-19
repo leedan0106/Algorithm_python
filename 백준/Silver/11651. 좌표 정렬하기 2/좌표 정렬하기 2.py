bfd45a69f8a356d3e989c632fa51c4ffcc8011e4
이다인 <63581721+leedan0106@users.noreply.github.com>
2024-04-19 10:42:57 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 344 ms, Memory: 59068 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,13 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = []
+for _ in range(n):
+  arr.append(list(map(int, input().split())))
+
+arr.sort(key=lambda x: (x[1], x[0]))
+
+for a in arr:
+  print(*a)
