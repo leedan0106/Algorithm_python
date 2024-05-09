2cefbfcfd380e0a4be964321a18ef9b5bf45cd5d
이다인 <63581721+leedan0106@users.noreply.github.com>
2024-05-09 22:42:27 +0900

    [Silver IV] Title: 상근이의 여행, Time: 156 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -0,0 +1,10 @@
+import sys
+input = sys.stdin.readline
+
+ts = int(input())
+
+for _ in range(ts):
+  n, m = map(int, input().split())
+  for _ in range(m):
+    a, b = map(int, input().split())
+  print(n-1)
